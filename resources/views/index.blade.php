393297229de6d44a857363fb2908ee41231a16f8
Arman <2001armanmaulana@gmail.com>
2022-07-07 17:50:10 +0800

    home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,117 @@
 <!doctype html>
 <html lang="en">
   <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Muhammad Arman Maulana</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400&display=swap" rel="stylesheet">
+    <title>Muhammad Arman Maulana</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    
+    <!-- nama website -->
+    <p class="text-center font-weight-bold underline mx-auto">HOME</p>
+    <!-- akhir nama website -->
+
+    <!-- section avatar-->
+    <div class="container-fluid row mt-5 px-5">
+      <div class="col-md px-5 py-4 mx-2 my-auto">
+        <h5 class="color-primary font-weight-bold">Hi there! 👋</h5>
+        <h1 class="font-weight-bold">I'm Muhammad Arman Maulana, a Software Engineer</h1>
+        <p class="color-text mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae vulputate in metus ac varius et id. Gravida fermentum ante augue orci diam interdum euismod. In pretium velit sed sed. Morbi odio metus, velit feugiat platea imperdiet sit sit. 
+        </p>
+        <button type="button" class="btn my-button mt-3 font-weight-semibold">Contact Me!</button>
+      </div>
+      <img src="img/avatar.png" alt="avatar" class="col-md">
+    </div>
+    <!-- section avatar-->
+
+    <!-- section about me -->
+    <div class="container-fluid row mt-5 px-5">
+      <div class="col-md px-5 py-4 mx-2 ">
+        <h5 class="color-primary font-weight-bold">About Me</h5>
+        <p class="text-justify color-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh quam senectus ultrices sit mattis ornare. Amet ac massa odio id viverra. Odio rhoncus vel consectetur vulputate ac. Ut gravida augue elementum eget in. Ut facilisis sit lectus quam in aenean congue eget. <br> Odio ipsum, tincidunt diam mauris. Feugiat sodales aliquam ut arcu dignissim adipiscing in egestas. Quis fermentum, tempus eu nulla. Pellentesque dictum ut fringilla dolor porta tincidunt. Odio ipsum, tincidunt diam mauris. Feugiat sodales aliquam ut arcu dignissim adipiscing in egestas. Quis fermentum, tempus eu nulla. Pellentesque dictum ut fringilla dolor porta tincidunt. <br> Odio ipsum, tincidunt diam mauris. Feugiat sodales aliquam ut arcu dignissim adipiscing in egestas. Quis fermentum, tempus eu nulla. Pellentesque dictum ut fringilla dolor porta tincidunt. tempus eu nulla. Pellentesque dictum ut fringilla dolor porta tincidunt.  tempus eu nulla. Pellentesque dictum ut fringilla dolor porta tincidunt. </p>
+      </div>
+      <div class="col-md ps-4 py-4 ms-3 ">
+        <h5 class="color-primary font-weight-bold">Skills</h5>
+        <div class="d-flex flex-row flex-wrap">
+          @php ($names = [
+            'html.png',
+            'css.png',
+            'flutter.png',
+            'firebase.png',
+            'javascript.png',
+            'nodejs.png',
+            'php.png',
+            'laravel.png',
+            'java.png',
+            'python.png',
+            'mysql.png',
+            'git.png',
+          ])
+          @foreach ($names as $icon)
+              <img src="img/{{$icon}}" alt="{{$icon}}" class="mr-3 mb-3" >
+          @endforeach
+        </div>
+      </div>
+    </div>
+    <!-- akhir about me -->
+    
+    <!-- experience section -->
+    <div class="container-fluid row mt-5 px-5">
+      <div class="col-md px-5 py-4 mx-2 ">
+        <h5 class="color-primary font-weight-bold mb-3">Education</h5>
+        <div class="row">
+          <div class="col-4">
+            <p class="color-text">2019 - Present</p>
+          </div>
+          <div class="col px-0">
+            <p class="color-text"> <span class="font-weight-bold text-white">Universitas Mulawarman</span>  <br>Major in Computer Science, current GPA 3.53/4.00.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-4">
+            <p class="color-text">Feb 2022 - Jul 2022</p>
+          </div>
+          <div class="col px-0">
+            <p class="color-text"> <span class="font-weight-bold text-white">Bangkit Academy Lead by Google, Gojek, Tokopedia and Traveloka</span>  <br>Enroll as Android Learning path student.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md ps-4 py-4 ms-3 ">
+        <h5 class="color-primary font-weight-bold mb-3">Organization</h5>
+        <p class="color-text "> <span class="font-weight-bold text-white">Staff at the Association of Informatics</span> <br>
+          Become a Public Relations Staff at the Association of Informatics and serves as a public relations officer. Handle Study Appeal. Got the award the most active and chosen to be the most outstanding staff.
+        </p>
+        <p class="color-text "> <span class="font-weight-bold text-white">Team GDSC Universitas Mulawarman</span> <br>
+          Become core team GDSC Mulawarman University. and served as event division.
+        </p>
+        <p class="color-text "> <span class="font-weight-bold text-white">Assistant Laboratory for OOP and Basic Programming Algorithm Subject</span> <br>
+          Has successfully organized, demonstrated, and taught practicum material for 2 class by being a good mentor for object-oriented programming and basic programming algorithm courses.
+        </p>
+        <p class="color-text "> <span class="font-weight-bold text-white">Assistant Laboratory for Mobile Programming</span> <br>
+          Has successfully organized, demonstrated, and taught practicum material for 6 class by being a great mentor for Mobile Programming using Flutter.
+        </p>
+      </div>
+    </div>
+    <!-- akhir experience -->
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+    -->
   </body>
 </html>
