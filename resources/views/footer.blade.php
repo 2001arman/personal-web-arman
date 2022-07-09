a31744d098bdc8ce426b90fc0c45e9e0a6d4524c
Arman <2001armanmaulana@gmail.com>
2022-07-09 11:42:48 +0800

    footer ui

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,7 @@
+<div class="container-fluid  px-5" id="footer-section">
+    <div style="height: 20px;"></div>
+    <div class="px-5" id="padding-footer">
+        <div class="divider container-fluid mt-5 px-5 mb-3"></div>
+        <p class="color-text text-center"> <span class="font-weight-bold color-primary"> MUHAMMAD ARMAN MAULANA </span> Copyright 2022 </p>
+    </div>
+</div>
