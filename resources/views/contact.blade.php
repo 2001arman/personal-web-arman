b235571101c087fe468a77fa647357df47032749
Arman <2001armanmaulana@gmail.com>
2022-07-09 11:42:40 +0800

    contact ui

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,23 @@
+<div class="container-fluid mt-5 px-5" id="contact-section">
+  <h5 class="color-primary font-weight-bold mb-4 mx-auto text-center">Contact Me</h5>
+  <div class="px-5" id="padding-big">
+    <div class="container-contact py-5 px-5" id="container-contact">
+      <form action="#" method="post" class="mx-5 px-3">
+        <input type="text" class="input-text py-2 px-4 mb-4 container-fluid text-white" placeholder="Name" required>
+        <input type="text" class="input-text py-2 px-4 mb-4 container-fluid text-white" placeholder="Email" required>
+        <textarea cols="30" rows="5" class="input-text py-2 px-4 mb-4 container-fluid text-white" placeholder="Message" required></textarea>
+        <button type="submit" class="btn my-button container-fluid font-weight-semibold">Contact Me</button>
+      </form>
+    </div>
+  </div>
+  <div class="d-flex justify-content-center mt-5">
+    <div class="divider my-auto" style="width: 114px;"></div>
+    <p class="color-text my-auto mx-3">or</p>
+    <div class="divider my-auto" style="width: 114px;"></div>
+  </div>
+  <div class="d-flex justify-content-center mt-3">
+    <a href="https://github.com/2001arman" target="_blank"> <img src="img/icon_github.png" alt="Github"> </a>
+    <a href="https://www.linkedin.com/in/muhammad-arman-maulana-359076215/" target="_blank"> <img src="img/icon_linkedin.png" alt="LinkedIn" class="mx-3"> </a>
+    <a href="https://www.instagram.com/armann_maulanaa/" target="_blank"> <img src="img/icon_instagram.png" alt="Instagram"> </a>
+  </div>
+</div>
