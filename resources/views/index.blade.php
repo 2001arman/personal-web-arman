2887b6b52e1d4e55b9fc8531e15bc2dd5ce133fe
Arman <2001armanmaulana@gmail.com>
2022-12-17 10:22:35 +0800

    edit about me section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,7 +32,7 @@
       <div class="col-sm px-5 py-4 mx-2 my-auto" id="container-avatar">
         <h5 class="color-primary font-weight-bold">Hi there! 👋</h5>
         <h1 class="font-weight-bold">I'm Muhammad Arman Maulana, a FullStack Developer</h1>
-        <p class="color-text mt-3">A FullStack Developer who specializes in mobile and web applications. In addition, Create a youtube channel called <a href="https://www.youtube.com/@flutternesia158" target="_blank" class="color-primary font-weight-bold text-underline hover-none">Flutternesia</a> and focus on creating content about flutter tutorial.
+        <p class="color-text mt-3">A FullStack Developer who specializes in mobile and web applications. In addition, i also create a youtube channel called <a href="https://www.youtube.com/@flutternesia158" target="_blank" class="color-primary font-weight-bold text-underline hover-none">Flutternesia</a> and focus on creating content about flutter tutorial.
         </p>
         <button type="button" class="btn my-button mt-3 font-weight-semibold" onclick="scrollToView()">Contact Me!</button>
       </div>
